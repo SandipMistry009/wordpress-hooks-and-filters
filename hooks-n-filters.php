2e0bb226f9ae4a887dc05201445400d85e5710fa
Sandip Mistry <mistry.sandi009@gmail.com>
2022-10-09 11:46:36 +0530

    Update hooks-n-filters.php
    Products search by SKU

--- hooks-n-filters.php
+++ hooks-n-filters.php
@@ -304,3 +304,34 @@ $discount = number_format(100, 2);
 $cart_object->add_fee( $label_text, -$discount, false );
 }
 }
+
+// Products search by SKU
+
+function search_by_sku( $search, &$query_vars ) {
+    global $wpdb;
+    if(isset($query_vars->query['s']) && !empty($query_vars->query['s'])){
+        $args = array(
+            'posts_per_page'  => -1,
+            'post_type'       => 'product',
+            'meta_query' => array(
+                array(
+                    'key' => '_sku',
+                    'value' => $query_vars->query['s'],
+                    'compare' => 'LIKE'
+                )
+            )
+        );
+        $posts = get_posts($args);
+        if(empty($posts)) return $search;
+        $get_post_ids = array();
+        foreach($posts as $post){
+            $get_post_ids[] = $post->ID;
+        }
+        if(sizeof( $get_post_ids ) > 0 ) {
+                $search = str_replace( 'AND (((', "AND ((({$wpdb->posts}.ID IN (" . implode( ',', $get_post_ids ) . ")) OR (", $search);
+        }
+    }
+    return $search;
+    
+}
+    add_filter( 'posts_search', 'search_by_sku', 999, 2 );
