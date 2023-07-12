86079bc57d0901c564c38f858038b27f4f4a5f5b
Sandip Mistry <mistry.sandi009@gmail.com>
2023-07-12 14:49:52 +0530

    Update woocommerce-hooks-and-filter.php
    IMG alt tag added by code

--- woocommerce-hooks-and-filter.php
+++ woocommerce-hooks-and-filter.php
@@ -285,3 +285,26 @@ function crunchify_clean_header_hook(){
 
 }
 add_action('init','crunchify_clean_header_hook');
+
+// images alt tag value added by programming
+
+add_filter('wp_get_attachment_image_attributes', 'change_attachement_image_attributes', 20, 2);
+
+function change_attachement_image_attributes( $attr, $attachment ){
+    // Get post parent
+    $parent = get_post_field( 'post_parent', $attachment);
+
+    // Get post type to check if it's product
+    $type = get_post_field( 'post_type', $parent);
+    if( $type != 'product' ){
+        return $attr;
+    }
+
+    /// Get title
+    $title = get_post_field( 'post_title', $parent);
+
+    $attr['alt'] = $title;
+    $attr['title'] = $title;
+
+    return $attr;
+}
