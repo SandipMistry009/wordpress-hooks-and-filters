25f9ddbe55c366852d2a1da07c5b1ee2c24bb106
Sandip Mistry <mistry.sandi009@gmail.com>
2023-07-12 11:56:01 +0530

    Update woocommerce-hooks-and-filter.php
    romove product and product_category from URL

--- woocommerce-hooks-and-filter.php
+++ woocommerce-hooks-and-filter.php
@@ -115,3 +115,52 @@ function devvn_product_category_rewrite_rules($flash = false) {
         flush_rewrite_rules(false);
 }
 add_action('init', 'devvn_product_category_rewrite_rules');
+
+// remove /product/ from URL
+
+function remove_product_slug( $post_link, $post, $leavename ) {
+    if ( 'product' != $post->post_type || 'publish' != $post->post_status ) {
+        return $post_link;
+    }
+    $post_link = str_replace( '/product/', '/', $post_link );
+    return $post_link;
+}
+add_filter( 'post_type_link', 'remove_product_slug', 10, 3 );
+
+function change_slug_struct( $query ) {
+
+    if ( ! $query->is_main_query() || 2 != count( $query->query ) || ! isset( $query->query['page'] ) ) {
+        return;
+    }
+
+    if ( ! empty( $query->query['name'] ) ) {
+        $query->set( 'post_type', array( 'post', 'product', 'page' ) );
+    } elseif ( ! empty( $query->query['pagename'] ) && false === strpos( $query->query['pagename'], '/' ) ) {
+        $query->set( 'post_type', array( 'post', 'product', 'page' ) );
+
+        // We also need to set the name query var since redirect_guess_404_permalink() relies on it.
+        $query->set( 'name', $query->query['pagename'] );
+    }
+}
+add_action( 'pre_get_posts', 'change_slug_struct', 99 );
+
+//  Remove /product_category/ from url and add .(dot) in permalink structure
+
+add_filter('request', function( $vars ) {
+    global $wpdb;
+    if( ! empty( $vars['pagename'] ) || ! empty( $vars['category_name'] ) || ! empty( $vars['name'] ) || ! empty( $vars['attachment'] ) ) {
+        $slug = ! empty( $vars['pagename'] ) ? $vars['pagename'] : ( ! empty( $vars['name'] ) ? $vars['name'] : ( !empty( $vars['category_name'] ) ? $vars['category_name'] : $vars['attachment'] ) );
+        $exists = $wpdb->get_var( $wpdb->prepare( "SELECT t.term_id FROM $wpdb->terms t LEFT JOIN $wpdb->term_taxonomy tt ON tt.term_id = t.term_id WHERE tt.taxonomy = 'product_cat' AND t.slug = %s" ,array( $slug )));
+        if( $exists ){
+            $old_vars = $vars;
+            $vars = array('product_cat' => $slug );
+            if ( !empty( $old_vars['paged'] ) || !empty( $old_vars['page'] ) )
+                $vars['paged'] = ! empty( $old_vars['paged'] ) ? $old_vars['paged'] : $old_vars['page'];
+            if ( !empty( $old_vars['orderby'] ) )
+                    $vars['orderby'] = $old_vars['orderby'];
+                if ( !empty( $old_vars['order'] ) )
+                    $vars['order'] = $old_vars['order'];    
+        }
+    }
+    return $vars;
+});
