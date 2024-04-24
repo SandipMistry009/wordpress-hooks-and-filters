154d4b835bbf3ff21dba5d3b2e2b64cbd0efe5a9
Sandip Mistry <mistry.sandi009@gmail.com>
2024-04-24 15:18:10 +0530

    Update hooks-n-filters.php
    disable unwanted thumbnail generation - woocommerce

--- hooks-n-filters.php
+++ hooks-n-filters.php
@@ -357,3 +357,23 @@ function hide_update_noticee_to_all_but_admin_users()
     }
 }
 add_action( 'admin_head', 'hide_update_noticee_to_all_but_admin_users', 1 );
+// disable unwanted thumbnail generation - woocommerce
+function add_image_insert_override($sizes){
+    //unset($sizes['thumbnail']);
+    unset($sizes['medium']);
+    unset($sizes['medium_large']);
+    unset($sizes['large']);
+    unset($sizes['1536x1536']);
+    unset($sizes['2048x2048']);        
+    unset($sizes['blog-isotope']);
+    unset($sizes['product_small_thumbnail']);
+    unset($sizes['shop_catalog']);
+    unset($sizes['shop_single']);
+    unset($sizes['shop_single_small_thumbnail']);
+    unset($sizes['shop_thumbnail']);
+    //unset($sizes['woocommerce_thumbnail']);
+    //unset($sizes['woocommerce_single']);
+    unset($sizes['woocommerce_gallery_thumbnail']);
+    return $sizes;
+}
+add_filter('intermediate_image_sizes_advanced', 'add_image_insert_override' );
